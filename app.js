67e822c710e550c7551c6b9942aa32ce95bd2872
DvKirbo <cuetosalazarsebas@gmail.com>
2025-03-17 17:10:49 -0500

    sortear, mostrar, agregar

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log('wasa')
+//lista vacia de amigos
+let friends = [];
+
+const friend = document.getElementById('amigo');
+const nameList = document.getElementById('listaAmigos');
+const resultado  = document.getElementById('resultado');
+
+function agregarAmigo() {
+    resultado.innerHTML = '';
+    if(friend.value === '') {
+        alert('Ingresa un nombre valido');
+        return;
+    }
+    if(!isNaN(friend.value)) {
+        alert('Ingresa un nombre valido');
+        return;
+    }
+    let nombre = friend.value;
+    friends.push(nombre);
+    mostrarAmigos();
+    friend.value = '';
+}
+
+function mostrarAmigos() {
+    nameList.innerHTML = '';
+    friends.forEach((friend) =>{
+        const item = document.createElement('li');
+        item.textContent = friend;
+        nameList.appendChild(item);
+    })
+}
+
+function sortearAmigo() {
+    nameList.innerHTML = '';
+    let random = Math.floor(Math.random() * friends.length);
+    resultado.textContent = `El amigo sorteado es: ${friends[random]}`;
+    friends = [];
+}
